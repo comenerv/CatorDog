66493b87f840c42513762cb71c5deb1d69041163
TzyWang <hua.wang.mo@gmail.com>
2020-08-18 02:07:39 -0400

    updated files folder

--- CNN.py
+++ CNN.py
@@ -9,4 +9,25 @@ import math
 from PIL import Image
 import tflearn
 from tflearn.data_utils import image_preloader
-%matplotlib inline
+#%matplotlib inline
+
+# data processing
+
+IMAGE_FOLDER = 'C:/temp/DogsandCats/train'
+VALIDATION_FOLDER = 'C:/temp/DogsandCats/test1'
+TRAIN_DATA = 'C:/temp/DogsandCats/train/training_data.txt'
+TEST_DATA = 'C:/temp/DogsandCats/train/test_data.txt'
+VALIDATION_DATA = 'C:/temp/DogsandCats/test1/validation_data.txt'
+train_porportion = 0.9
+test_porportion = 0.1
+#validation_proportion = 0.1
+
+filenames_image = os.listdir(IMAGE_FOLDER)
+random.shuffle(filenames_image)
+
+total = len(filenames_image)
+fr = open(TRAIN_DATA, 'w')
+train_files = filenames_image[0: int(train_porportion*total)]
+for filename in train_files:
+    if filenames[0:3] == 'cat':
+        fr.write()
